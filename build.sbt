d8a8b93d776b39cc64d5efd2043b1102378b0407
xstahol <staffan.holmgren@tre.se>
2017-05-05 13:57:30 +0200

    2017-05-01 challenge

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "challanges"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+        
---
+++ src/main/scala/Challenge_2017_05_01.scala
@@ -0,0 +1,33 @@
+
+
+
+/**
+  * Given a sorted list of distinct integers, write a function that returns whether there are two integers in the list that add up to 0. For example, you would return true if both -14435 and 14435 are in the list, because -14435 + 14435 = 0. Also return true if 0 appears in the list.
+  * https://www.reddit.com/r/dailyprogrammer/comments/68oda5/20170501_challenge_313_easy_subset_sum/?st=j2bsc6u1&sh=f90a1458
+  */
+object Challenge_2017_05_01 extends App {
+
+  def negativeValueEqualsZero(numbers: List[Int]): Boolean = {
+    for (number <- numbers) {
+      if (numbers.contains(number * -1))
+        return true
+    }
+    false
+  }
+
+  val true_1 = List(-97364, -71561, -69336, 19675, 71561, 97863)
+  val true_2 = List(-1, 1)
+  val true_3 = List(-53974, -39140, -36561, -23935, -15680, 0)
+
+  val false_1 = List(-5, -3, -1, 2, 4, 6)
+  val false_2 = List(1, 2, 3)
+  val false_3 = List()
+
+  println(false_1 + " -> " + negativeValueEqualsZero(false_1))
+  println(false_2 + " -> " + negativeValueEqualsZero(false_2))
+  println(false_3 + " -> " + negativeValueEqualsZero(false_3))
+
+  println(true_1 + " -> " + negativeValueEqualsZero(true_1))
+  println(true_2 + " -> " + negativeValueEqualsZero(true_2))
+  println(true_3 + " -> " + negativeValueEqualsZero(true_3))
+}
