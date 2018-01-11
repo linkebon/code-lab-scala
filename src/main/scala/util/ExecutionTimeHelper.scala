44b9cd4c67ef86d9fb92a48745954a68091f7410
xstahol <staffan.holmgren@tre.se>
2018-01-11 21:18:43 +0100

    Formatting

--- src/main/scala/util/ExecutionTimeHelper.scala
+++ src/main/scala/util/ExecutionTimeHelper.scala
@@ -2,9 +2,12 @@ package util
 
 trait ExecutionTimeHelper {
   def time(block: => Unit): Unit = {
+    println("####################################################")
+
     val t0 = System.currentTimeMillis()
     block
     val t1 = System.currentTimeMillis()
     println(s"Execution time: ${t1 - t0} milliseconds")
+    println("####################################################")
   }
 }
