2dcd177db5ab074f11262b7c45a619d4aa656637
xstahol <staffan.holmgren@tre.se>
2018-01-11 19:57:12 +0100

    You want to build a word cloud, an infographic where the size of a word corresponds to how often it appears in the body of text.
    Goes through input string multiple times

---
+++ src/main/scala/interviewcake_34.scala
@@ -0,0 +1,24 @@
+import scala.collection.{immutable, mutable}
+
+object Test extends App {
+  val dictionary = mutable.Map[String, Int]()
+  val notWantedChars = Array(';', ':', '.', ',', '\'', '(', ')', '\'')
+  var modifiedText = ""
+  "We came, we saw, we conquered...then we ate Bill's (Mille-Feuille) cake. The bill came to five dollars."
+    .foreach {
+      case char if notWantedChars.contains(char) => modifiedText += " "
+      case char => modifiedText += char
+    }
+
+  modifiedText
+    .split(" ")
+    .filterNot(word => word.length == 1 || " ".equalsIgnoreCase(word))
+    .foreach { word =>
+      if (dictionary.contains(word)) {
+        dictionary(word) = dictionary(word) + 1
+      } else {
+        dictionary += word -> 1
+      }
+    }
+  dictionary.foreach(println)
+}
