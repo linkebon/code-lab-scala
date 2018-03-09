6be0a898e81843dfbd26431d1d49b67c6cce1223
xstahol <staffan.holmgren@tre.se>
2018-03-09 11:39:50 +0100

    Pyramid of numbers in scala

--- src/main/scala/scala/pyramids/Pyramid_of_numbers_medium2.scala
+++ src/main/scala/scala/pyramids/Pyramid_of_numbers_medium2.scala
@@ -4,16 +4,15 @@ import scala.io.StdIn
 
 /**
    Rows in pyramid: 9
-                  1
-                1 2 1
-              1 2 3 2 1
-            1 2 3 4 3 2 1
-          1 2 3 4 5 4 3 2 1
-        1 2 3 4 5 6 5 4 3 2 1
-      1 2 3 4 5 6 7 6 5 4 3 2 1
-    1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
-  1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
-
+                 1
+               1 2 1
+             1 2 3 2 1
+           1 2 3 4 3 2 1
+         1 2 3 4 5 4 3 2 1
+       1 2 3 4 5 6 5 4 3 2 1
+     1 2 3 4 5 6 7 6 5 4 3 2 1
+   1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+ 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
   */
 object Pyramid_of_numbers_medium2 extends App {
   print("Rows in pyramid: ")
@@ -21,7 +20,7 @@ object Pyramid_of_numbers_medium2 extends App {
   var currentRow = 1
 
   for (n <- pyramidRows to 1 by -1) {
-    (1 to n * 2).foreach(_ => print(" "))
+    (1 until n * 2).foreach(_ => print(" "))
     (1 until currentRow).foreach(iteration => print(iteration + " "))
     (currentRow to 1 by -1).foreach(iteration => print(iteration + " "))
     println()
