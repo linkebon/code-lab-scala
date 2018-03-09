fbbf7aa58bc39d01bff8341b95440538d3db8ee3
xstahol <staffan.holmgren@tre.se>
2018-03-09 11:37:35 +0100

    Pyramid of numbers in scala

--- src/main/scala/scala/pyramids/Pyramid_of_numbers_medium2.scala
+++ src/main/scala/scala/pyramids/Pyramid_of_numbers_medium2.scala
@@ -2,7 +2,19 @@ package scala.pyramids
 
 import scala.io.StdIn
 
+/**
+   Rows in pyramid: 9
+                  1
+                1 2 1
+              1 2 3 2 1
+            1 2 3 4 3 2 1
+          1 2 3 4 5 4 3 2 1
+        1 2 3 4 5 6 5 4 3 2 1
+      1 2 3 4 5 6 7 6 5 4 3 2 1
+    1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+  1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
 
+  */
 object Pyramid_of_numbers_medium2 extends App {
   print("Rows in pyramid: ")
   val pyramidRows = StdIn.readInt()
